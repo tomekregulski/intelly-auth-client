d3a9cfdaed7d766576d24dcedefa0fb08c4dfd09
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-10-16 10:47:04 -0400

    Testing JWT query

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,35 +31,33 @@ const Login = () => {
 
       console.log(payload);
 
-      return (
-        axios
-          // .post('https://intelly-auth-service.herokuapp.com/api/users/login', {
-          .post('http://localhost:5000/api/users/login', {
-            payload,
-          })
-          .then((response) => {
-            if (response.data.token) {
-              localStorage.setItem('user', JSON.stringify(response.data));
-            }
+      return axios
+        .post('https://intelly-auth-service.herokuapp.com/api/users/login', {
+          // .post('http://localhost:5000/api/users/login', {
+          payload,
+        })
+        .then((response) => {
+          if (response.data.token) {
+            localStorage.setItem('user', JSON.stringify(response.data));
+          }
 
-            setIsAuth(true);
-            console.log(response.data);
+          setIsAuth(true);
+          console.log(response.data);
 
-            setUserData({
-              id: response.data.id,
-              email: response.data.email,
-              password: response.data.password,
-              first_name: response.data.first_name,
-              last_name: response.data.last_name,
-              roles: response.data.roles,
-              access: response.data.access,
-              brands: response.data.brands,
-              token: response.data.token,
-            });
+          setUserData({
+            id: response.data.id,
+            email: response.data.email,
+            password: response.data.password,
+            first_name: response.data.first_name,
+            last_name: response.data.last_name,
+            roles: response.data.roles,
+            access: response.data.access,
+            brands: response.data.brands,
+            token: response.data.token,
+          });
 
-            history.push('/');
-          })
-      );
+          history.push('/');
+        });
     }
   };
 
